4533d5222737fce4b6c8fb1ce216b143dc407627
charleshuy <150873986+charleshuy@users.noreply.github.com>
2024-04-16 18:53:56 +0700

    Add files via upload

---
+++ Session01-HelloWorld/HelloCSharp/BMICalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloCSharp;
+public class BMICalculator
+{
+    	
+    public static double GetBMI(double weight, double height)
+    {
+        return weight / (height * height);
+    }
+}
---
+++ Session01-HelloWorld/HelloCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloCSharp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Happy new year!!");
+	double bmi = BMICalculator.GetBMI(63, 1.68);
+	Console.WriteLine("And your BMI is: " + bmi);
+	Console.WriteLine("Press any key to exit!!!");
+	Console.ReadLine();
+    }
+}
